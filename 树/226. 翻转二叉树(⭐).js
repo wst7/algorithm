d012411204eb1b5a3c68484ca5dcbec668f332b4
wstreet <wstreet7@outlook.com>
2021-03-20 18:26:24 +0800

    226. 翻转二叉树(⭐)

---
+++ 树/226. 翻转二叉树(⭐).js
@@ -0,0 +1,25 @@
+// https://leetcode-cn.com/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+ var invertTree = function(root) {
+  if (root === null) {
+      return root
+  }
+  const temp = root.left
+  root.left = root.right
+  root.right = temp
+  invertTree(root.left)
+  invertTree(root.right)
+  return root
+};
