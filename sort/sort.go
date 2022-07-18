eafb29060dce644720a34576bd3eb4f5b14fbd08
wstreet <wstreet7@outlook.com>
2022-07-19 00:02:22 +0800

    select sert and quick sort with go
    Change-Id: Ie0559074b98bb5a5e0bb4a320543344b8966682d

--- sort/sort.go
+++ sort/sort.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func swap(arr []int, left int, right int) {
 	rightV := arr[right]
@@ -45,8 +48,56 @@ func insertSort(arr []int) {
 	fmt.Println("结果：", arr)
 }
 
+// 3.选择排序
+// 选择要放置元素的位置，将其与其他元素进行比较大小，判断是否要交换
+func selectSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		fmt.Println("第", i, "个位置选择排序")
+		for j := i + 1; j < n; j++ {
+			if arr[i] > arr[j] {
+				swap(arr, i, j)
+			}
+		}
+		fmt.Println(arr)
+	}
+	fmt.Println("结果：", arr)
+}
+
+// 4.快速排序
+// 选择基准元素，把小于基准元素的都放在基准元素左边，大于基准元素的都放在基准元素右边
+// 左右两边的子集重复上述动作
+
+func remove(slice []int, s int) []int {
+	return append(slice[:s], slice[s+1:]...)
+}
+func quickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	pIndex := int(math.Floor(float64(len(arr) / 2)))
+	pItem := arr[pIndex]
+	arr = remove(arr, pIndex)
+	left, right, result := []int{}, []int{}, []int{}
+	for i := 0; i < len(arr); i++ {
+		if arr[i] < pItem {
+			left = append(left, arr[i])
+		} else {
+			right = append(right, arr[i])
+		}
+	}
+	result = append(result, quickSort(left)...)
+	result = append(result, pItem)
+	result = append(result, quickSort(right)...)
+	fmt.Println(result)
+	return result
+}
+
 func main() {
-	var arr = []int{3, 4, 2, 5, 1}
+	var arr = []int{3, 4, 2, 5, 1, 9, 8, 7}
 	// bubbleSort(arr)
-	insertSort(arr)
+	// insertSort(arr)
+	// selectSort(arr)
+	res := quickSort(arr)
+	fmt.Println(res)
 }
