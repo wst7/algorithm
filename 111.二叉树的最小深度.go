0c2229cff2141ce632d76cf2bb896083613541be
wstreet7 <wstreet7@outlook.com>
2024-03-27 09:36:11 +0800

    feat: 二叉树的最小深度

---
+++ 111.二叉树的最小深度.go
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=golang
+ *
+ * [111] 二叉树的最小深度
+ *
+ * https://leetcode.cn/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (53.55%)
+ * Likes:    1169
+ * Dislikes: 0
+ * Total Accepted:    665.1K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最小深度。
+ *
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ *
+ * 说明：叶子节点是指没有子节点的节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：2
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [2,null,3,null,4,null,5,null,6]
+ * 输出：5
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数的范围在 [0, 10^5] 内
+ * -1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+	return getMinDepth(root)
+
+}
+func getMinDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	if root.Left == nil {
+		return 1 + getMinDepth(root.Right)
+	}
+	if root.Right == nil {
+		return 1 + getMinDepth(root.Left)
+	}
+	return 1 + min(getMinDepth(root.Left), getMinDepth(root.Right))
+
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
+
